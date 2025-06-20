342bf4999c43b2e6624fd485e9b9c7a662df15db
Randall71 <51382079+Randall71@users.noreply.github.com>
2025-06-20 09:33:37 +0000

    chore: fix ios project

--- example/ios/TectoysunmisdkExample/AppDelegate.swift
+++ example/ios/TectoysunmisdkExample/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "NavigationModeExample",
+      withModuleName: "TectoysunmisdkExample",
       in: window,
       launchOptions: launchOptions
     )
